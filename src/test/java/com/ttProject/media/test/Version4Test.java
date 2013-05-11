d25c58d282c9cb064d437d7a12491b91358e0bf2
Toda Takahiko <poepoemix@hotmail.com>
2013-05-12 08:45:16 +0900

    音声の部分だけ抽出してmp4をつくる動作テスト

--- src/test/java/com/ttProject/media/test/Version4Test.java
+++ src/test/java/com/ttProject/media/test/Version4Test.java
@@ -1,12 +1,19 @@
 package com.ttProject.media.test;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 
 import com.ttProject.library.BufferUtil;
 import com.ttProject.media.mp4.Atom;
 import com.ttProject.media.mp4.IAtomAnalyzer;
+import com.ttProject.media.mp4.ParentAtom;
+import com.ttProject.media.mp4.atom.Co64;
 import com.ttProject.media.mp4.atom.Dinf;
 import com.ttProject.media.mp4.atom.Ftyp;
 import com.ttProject.media.mp4.atom.Hdlr;
@@ -43,17 +50,25 @@ public class Version4Test {
 		'M', '4', 'A', ' ',
 		'm', 'p', '4', '2'
 	};
+	private String target = "http://49.212.39.17/mario.mp4";
+	private String output = "output.mp4";
+	private String tmp = "output.idx"; // 新旧長さ
+	private Stsc stsc = null;
+	private Stsz stsz = null;
+	private Stco stco = null;
+	private Co64 co64 = null;
 	@Test
 	public void test() throws Exception {
-		// ここで動作を実施します。
-		// とりあえずthreadの中身が実行することをここでやっとく。
-		IFileReadChannel fc = FileReadChannel.openFileReadChannel("http://49.212.39.17/mario.mp4");
-		// 解析動作
+		// 前の出力があったら邪魔なので削除する。
+		new File(output).delete();
+		// 読み込みターゲットを開く
+		IFileReadChannel fc = FileReadChannel.openFileReadChannel(target);
+
+		// 解析処理
 		IAtomAnalyzer analyzer = new IAtomAnalyzer() {
 			@Override
 			public Atom analyze(IFileReadChannel ch) throws Exception {
 				if(ch.size() == ch.position()) {
-					// もうデータがない
 					return null;
 				}
 				int position = ch.position();
@@ -191,50 +206,278 @@ public class Version4Test {
 				}
 				if("mdat".equals(tag)) {
 					Mdat mdat = new Mdat(size, position);
-//					ch.position(position + size);
-					return null;
+					// mdatの位置を考えることでmoovが後ろにあるmp4でも対応できるようになる。(ただし処理がおそくなる)
+					ch.position(position + size);
+					return mdat;
 				}
+				// TODO freeという余計なタグがある可能性あり。
 				System.out.println(tag);
 				return null;
 			}
 		};
 		Atom atom = null;
-		Ftyp ftyp = null;
 		Moov moov = null;
-		Mdat mdat = null;
 		while((atom = analyzer.analyze(fc)) != null) {
-			if(atom instanceof Ftyp) {
-				ftyp = (Ftyp)atom;
-			}
 			if(atom instanceof Moov) {
-				moov = (Moov)atom;
+				moov = (Moov)atom; // moovの部分だけ知りたい
 			}
-			System.out.println(atom);
+		}
+		// ここまでatomの準備できあがり
+		// サイズを確認してファイルをつくっていきます。
+		List<Atom> list = new ArrayList<Atom>();
+		int pos = moovAtoms(moov, list, Version4Test.ftyp.length + 8);
+		// このタイミングですでにstscとstszのコピーははじまっている。
+		// あとは本体のコピーを実施する必要あり。
+		try {
+			// 新しいファイルをつくりはじめる。
+			FileChannel target = new FileOutputStream(output, true).getChannel();
+			ByteBuffer buffer = null;
+			// ftyp
+			buffer = ByteBuffer.allocate(Version4Test.ftyp.length + 8);
+			buffer.put(Version4Test.ftyp);
+			// moov
+			buffer.putInt(pos - Version4Test.ftyp.length);
+			buffer.put("moov".getBytes());
+			buffer.flip();
+			target.write(buffer);
+			// 通常のタグを書き込む
+			for(Atom aa : list) {
+				if(aa instanceof ParentAtom && !(aa instanceof Dinf)) {
+					buffer = ByteBuffer.allocate(8);
+					buffer.putInt(aa.getSize());
+					buffer.put(aa.getName().getBytes());
+					buffer.flip();
+					target.write(buffer);
+				}
+				else {
+					aa.copy(fc, target);
+				}
+			}
+			// stcoを構成したい。
+			FileChannel idx = new FileOutputStream(tmp).getChannel();
+			if(stco != null) {
+				// stcoから16バイトコピーしておく。
+				fc.position(stco.getPosition());
+				buffer = BufferUtil.safeRead(fc, 16);
+				target.write(buffer);
+				buffer.position(12);
+				int stcoCount = buffer.getInt();
+				buffer = ByteBuffer.allocate(8);
+				buffer.putInt(0);
+				buffer.putInt(stcoCount);
+				buffer.flip();
+				idx.write(buffer);
+//				target.write(BufferUtil.safeRead(fc, 16));
+				pos += 8; // データの開始位置をいれておく。
+			}
+			else {
+				throw new RuntimeException("stcoがない");
+			}
+			IFileReadChannel stscReader = FileReadChannel.openFileReadChannel(this.target);
+			IFileReadChannel stszReader = FileReadChannel.openFileReadChannel(this.target);
+			stscReader.position(stsc.getPosition() + 12);
+			int stscCount = BufferUtil.safeRead(stscReader, 4).getInt();
+			stszReader.position(stsz.getPosition() + 12);
+			// stszの値はとりあえず無視しておく。(ただしいと思っておく)
+			int stszConstant = BufferUtil.safeRead(stszReader, 4).getInt();
+			int stszCount = BufferUtil.safeRead(stszReader, 4).getInt();
+//			System.out.println(Integer.toHexString(stscCount));
+//			System.out.println(Integer.toHexString(stszConstant));
+//			System.out.println(Integer.toHexString(stszCount));
+			// stcoの内容を構築していきます。(co64の場合もあり)
+			// 元のデータがどこからか？という情報も必要(アクセス用)
+			int chunkTotal = 0;
+			int currentChunk = 1;
+			int currentSampleCount = 0;
+			for(int i = 0;i < stscCount;i ++) {
+				// stscのあるかぎり読み込む
+				buffer = BufferUtil.safeRead(stscReader, 12);
+				int chunk = buffer.getInt();
+				for(;currentChunk < chunk;currentChunk ++) {
+					// 各チャンク用のsampleの大きさを計算する必要あり。
+					int chunkSize = 0;
+					for(int j = 0;j < currentSampleCount;j ++) {
+						chunkSize += BufferUtil.safeRead(stszReader, 4).getInt();
+					}
+					// 位置情報を書き込みます
+					ByteBuffer buf = ByteBuffer.allocate(4);
+					buf.putInt(pos);
+					buf.flip();
+					target.write(buf);
+					buf.position(0);
+					idx.write(buf);
+					idx.write(BufferUtil.safeRead(fc, 4));
+					buf = ByteBuffer.allocate(4);
+					buf.putInt(chunkSize);
+					buf.flip();
+					idx.write(buf);
+					pos += chunkSize;
+					chunkTotal += chunkSize;
+				}
+				currentSampleCount = buffer.getInt();
+				buffer.getInt();
+			}
+			// 最後まできたら、最後のデータを書き込んでおく
+			// 各チャンク用のsampleの大きさを計算する必要あり。
+			int chunkSize = 0;
+			for(int j = 0;j < currentSampleCount;j ++) {
+				chunkSize += BufferUtil.safeRead(stszReader, 4).getInt();
+			}
+			ByteBuffer buf = ByteBuffer.allocate(4);
+			buf.putInt(pos);
+			buf.flip();
+			target.write(buf);
+			buf.position(0);
+			idx.write(buf);
+			idx.write(BufferUtil.safeRead(fc, 4));
+			buf = ByteBuffer.allocate(4);
+			buf.putInt(chunkSize);
+			buf.flip();
+			idx.write(buf);
+			pos += chunkSize;
+			chunkTotal += chunkSize;
+
+			stscReader.close();
+			stszReader.close();
+			// とりあえずここからは、mdatの生成が必要。
+			// 読み込みデータ量をしっておく。
+			buffer = ByteBuffer.allocate(8);
+			buffer.putInt(chunkTotal);
+			buffer.put("mdat".getBytes());
+			buffer.flip();
+			target.write(buffer);
+			idx.close();
+
+			// mdatの内容を書いておきたい。
+			IFileReadChannel index = FileReadChannel.openFileReadChannel(tmp);
+			index.position(8);
+			while(true) {
+				if(index.size() == index.position()) {
+					break;
+				}
+				buffer = BufferUtil.safeRead(index, 12);
+				buffer.position(4);
+				fc.position(buffer.getInt());
+				BufferUtil.quickCopy(fc, target, buffer.getInt());
+			}
+			index.close();
+			target.close();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
 		}
 		fc.close();
-		System.out.println("ftyp:1C");
-		System.out.println("moov:8"); // header部
-		for(Atom at : moov.getAtoms()) {
-			if(at instanceof Mvhd) {
-				System.out.println("  mvhd:" + Integer.toHexString(at.getSize()));
-				continue;
-			}
-			if(at instanceof Trak) {
-				Trak t = (Trak)at;
-				Tkhd tkhd = null;
-				for(Atom att : t.getAtoms()) {
-					if(att instanceof Tkhd) {
-						tkhd = (Tkhd)att;
-					}
-				}
-				if(tkhd == null) {
-					// 動画データなのでパスする。
+		System.out.println("おわり");
+	}
+	// atomをソートして必要なもののみでてくるようにする。
+	private int moovAtoms(Moov moov, List<Atom> list, int pos) {
+		Trak trak = null;
+		for(Atom atom : moov.getAtoms()) {
+			if(atom instanceof Trak) {
+				// trakは次につなげる命令なので最後に足す
+				Trak t = (Trak)atom;
+				if(t.getAtoms().size() == 0) {
+					// 映像のあるatomなので無視
 					continue;
 				}
-				System.out.println("  trak:" + Integer.toHexString(at.getSize()));
+				trak = t;
+				continue;
 			}
-//			System.out.println(at);
+			else if(atom instanceof Iods){
+				// iodsは拡張命令なので無視する。
+				continue;
+			}
+			else if(atom instanceof Udta){
+				// iodsは拡張命令なので無視する。
+				continue;
+			}
+			pos += atom.getSize();
+			list.add(atom);
 		}
-		// ここまででmoovのサイズが決定します。
+		if(trak != null) {
+			list.add(trak);
+			pos += 8;
+			pos = trakAtoms(trak, list, pos);
+		}
+		return pos;
+	}
+	private int trakAtoms(Trak trak, List<Atom> list, int pos) {
+		Mdia mdia = null;
+		for(Atom atom : trak.getAtoms()) {
+			if(atom instanceof Mdia) {
+				mdia = (Mdia)atom;
+				continue;
+			}
+			pos += atom.getSize();
+			list.add(atom);
+		}
+		if(mdia != null) {
+			list.add(mdia);
+			pos += 8;
+			pos = mdiaAtoms(mdia, list, pos);
+		}
+		return pos;
+	}
+	private int mdiaAtoms(Mdia mdia, List<Atom> list, int pos) {
+		Minf minf = null;
+		for(Atom atom : mdia.getAtoms()) {
+			if(atom instanceof Minf) {
+				minf = (Minf)atom;
+				continue;
+			}
+			pos += atom.getSize();
+			list.add(atom);
+		}
+		if(minf != null) {
+			list.add(minf);
+			pos += 8;
+			pos = minfAtoms(minf, list, pos);
+		}
+		return pos;
+	}
+	private int minfAtoms(Minf minf, List<Atom> list, int pos) {
+		Stbl stbl = null;
+		for(Atom atom : minf.getAtoms()) {
+			if(atom instanceof Stbl) {
+				stbl = (Stbl)atom;
+				continue;
+			}
+			pos += atom.getSize();
+			list.add(atom);
+		}
+		if(stbl != null) {
+			list.add(stbl);
+			pos += 8;
+			pos = stblAtoms(stbl, list, pos);
+		}
+		return pos;
+	}
+	private int stblAtoms(Stbl stbl, List<Atom> list, int pos) {
+		for(Atom atom : stbl.getAtoms()) {
+			if(atom instanceof Stsc) {
+				stsc = (Stsc) atom;
+			}
+			if(atom instanceof Stsz) {
+				stsz = (Stsz)atom;
+			}
+			if(atom instanceof Stco) {
+				stco = (Stco)atom;
+				continue;
+			}
+			if(atom instanceof Co64) {
+				co64 = (Co64)atom;
+				continue;
+			}
+			pos += atom.getSize();
+			list.add(atom);
+		}
+		if(stco != null) {
+			// 情報表示
+			pos += stco.getSize();
+		}
+		if(co64 != null) {
+			pos += co64.getSize();
+		}
+		return pos;
 	}
 }
