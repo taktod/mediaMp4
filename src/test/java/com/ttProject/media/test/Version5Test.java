187caaf1bd998eb2c09e82b403043ca73fed63a7
taktod <poepoemix@hotmail.com>
2013-05-26 10:59:10 +0900

    とりあえず、ローカルにあるmp4をflv(映像のみ)に変換するプログラムを作成 timescaleを考慮していないので、そこはなおさないとだめ。

--- src/test/java/com/ttProject/media/test/Version5Test.java
+++ src/test/java/com/ttProject/media/test/Version5Test.java
@@ -3,17 +3,27 @@ package com.ttProject.media.test;
 import java.io.FileOutputStream;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.channels.WritableByteChannel;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
 
+import com.ttProject.media.flv.FlvHeader;
 import com.ttProject.media.mp4.Atom;
 import com.ttProject.media.mp4.IAtomAnalyzer;
 import com.ttProject.media.mp4.atom.Moov;
+import com.ttProject.media.mp4.atom.Stco;
+import com.ttProject.media.mp4.atom.Stsc;
+import com.ttProject.media.mp4.atom.Stss;
+import com.ttProject.media.mp4.atom.Stsz;
+import com.ttProject.media.mp4.atom.Stts;
 import com.ttProject.media.mp4.atom.Trak;
 import com.ttProject.media.version5.AtomAnalyzer;
 import com.ttProject.media.version5.IndexFileCreator;
+import com.ttProject.media.version5.Msh;
+import com.ttProject.media.version5.Sond;
+import com.ttProject.media.version5.Vdeo;
 import com.ttProject.nio.channels.FileReadChannel;
 import com.ttProject.nio.channels.IFileReadChannel;
 import com.ttProject.util.BufferUtil;
@@ -71,7 +81,7 @@ public class Version5Test {
 	public void analyzeTest() throws Exception {
 //		IFileReadChannel fc = FileReadChannel.openFileReadChannel("http://49.212.39.17/test.mp4");
 		IFileReadChannel fc = FileReadChannel.openFileReadChannel(
-				Thread.currentThread().getContextClassLoader().getResource("mario2.mp4")
+			Thread.currentThread().getContextClassLoader().getResource("mario2.mp4")
 		);
 		IndexFileCreator analyzer = new IndexFileCreator("output.tmp");
 		Atom atom = null;
@@ -95,28 +105,133 @@ public class Version5Test {
 	private void makeupFlv(IFileReadChannel source, IFileReadChannel tmp) throws Exception {
 		// tmpファイルからVdeoとSondを取り出す。
 		while(tmp.position() < tmp.size()) {
-			System.out.println("pos:" +tmp.position());
-			System.out.println("size:" + tmp.size());
 			int position = tmp.position();
 			ByteBuffer buffer = BufferUtil.safeRead(tmp, 8);
 			int size = buffer.getInt();
-			System.out.println(size);
 			String tag = BufferUtil.getDwordText(buffer);
 			if("vdeo".equals(tag)) {
-				System.out.println("vdeo");
 				vdeo = new Vdeo(size, position);
 				vdeo.analyze(tmp);
 				tmp.position(position + size);
 			}
 			else if("sond".equals(tag)) {
-				System.out.println("sond");
 				sond = new Sond(size, position);
 				sond.analyze(tmp);
 				tmp.position(position + size);
 			}
-			System.out.println("nextPos:" + position + size);
 		}
-		System.out.println(vdeo);
-		System.out.println(sond);
+		// ここから先データを取り出して調整します。
+		System.out.println("videoつくるよ");
+		makeVideo(source, tmp);
+	}
+	private FileChannel output;
+	private void makeVideo(IFileReadChannel source, IFileReadChannel tmp) throws Exception {
+		output = new FileOutputStream("target.flv").getChannel();
+		// headerを書き込む
+		FlvHeader flvHeader = new FlvHeader();
+		flvHeader.setVideoFlg(true);
+		flvHeader.setAudioFlg(false);
+		flvHeader.write(output);
+		// mediaSequenceHeaderを書き込む
+		// 09 size 00 00 00 00 00 00 00 17 00 00 00 00 data endsize
+		System.out.println("mshつくる");
+		Msh msh = vdeo.getMsh();
+		tmp.position(msh.getPosition() + 8);
+		writeVideoTag(output, 0, 0, tmp, msh.getSize() - 8);
+		System.out.println("here...");
+		// データを書き込む
+		// stssの場合はkeyFrame
+		// stcoから位置をしる。
+		vdeo.getStco().start(tmp, false); // dataPos
+		vdeo.getStsc().start(tmp, false); // samples in chunk
+		vdeo.getStsz().start(tmp, false); // sample size
+		vdeo.getStss().start(tmp, false); // keyFrame
+		vdeo.getStts().start(tmp, false); // time
+		System.out.println("初期化おわり");
+		int timePos = 0;
+		int sampleCount = 0;
+		vdeo.getStss().nextKeyFrame(); // 次のキーフレーム番号を取得しておく。
+		System.out.println("ここきた。");
+		// を延々と書き続けていけばよさそう。
+		while(vdeo.getStco().nextChunkPos() != -1) {
+			System.out.println("こうなった");
+			// chunkデータがまだある場合
+			source.position(vdeo.getStco().getChunkPos());
+			vdeo.getStsc().nextChunk();
+			int chunkSampleCount = vdeo.getStsc().getSampleCount();
+			System.out.println(chunkSampleCount);
+			for(int i = 0;i < chunkSampleCount;i ++) {
+				// 読み込むサイズを知る。
+				int sampleSize = vdeo.getStsz().nextSampleSize();
+				if(sampleSize == -1) {
+					break;
+				}
+				sampleCount ++;
+				System.out.println("sampleCount:" + sampleCount);
+				// ここでデータがわかるので、追記しておく
+				if(vdeo.getStss().getKeyFrame() == sampleCount) {
+					writeVideoTag(output, timePos, 1, source, sampleSize);
+					vdeo.getStss().nextKeyFrame();
+				}
+				else {
+					writeVideoTag(output, timePos, 2, source, sampleSize);
+				}
+				int delta = vdeo.getStts().nextDuration();
+				System.out.println("delta:" + delta);
+				if(delta == -1) {
+					break;
+				}
+				timePos += delta;
+			}
+		}
+		output.close();
+	}
+	/**
+	 * 動画データを書き込む
+	 * @param target 書き込み先
+	 * @param timestamp timestamp値
+	 * @param keyFrame キーフレームであるか? 1:keyFrame 0:msh 2:innerFrame
+	 * @param source データ元のIFileReadChannel
+	 * @param size データ元での読み込みバイト数
+	 */
+	private void writeVideoTag(WritableByteChannel target, int timestamp, int keyFrame, IFileReadChannel source, int size) throws Exception {
+		// h.264の場合はsize + 5バイト、余計にデータが必要
+		// 09 size timestamp 00 00 00 17 00 00 00 00 データ eof(msh)
+		// 09 size timestamp 00 00 00 17 01 00 00 00 データ eof(keyFrame)
+		// 09 size timestamp 00 00 00 27 01 00 00 00 データ eof(innerFrame)
+		ByteBuffer buffer = ByteBuffer.allocate(11 + 5);
+		buffer.putInt((size + 5) | 0x09000000);
+		// timestamp
+		buffer.put((byte)((timestamp >> 16) & 0xFF));
+		buffer.put((byte)((timestamp >> 8) & 0xFF));
+		buffer.put((byte)((timestamp >> 0) & 0xFF));
+		buffer.put((byte)((timestamp >> 24) & 0xFF));
+		switch(keyFrame) {
+		case 0:
+			buffer.putInt(0x17);
+			buffer.putInt(0);
+			break;
+		case 1:
+			buffer.putInt(0x17);
+			buffer.putInt(0x01000000);
+			break;
+		case 2:
+			buffer.putInt(0x27);
+			buffer.putInt(0x01000000);
+			break;
+		default:
+			break;
+		}
+		buffer.flip();
+		output.write(buffer);
+		System.out.println("timestamp:" +timestamp);
+		System.out.println("pos:" + source.position());
+		System.out.println("size:" + size);
+		buffer = BufferUtil.safeRead(source, size);
+		output.write(buffer);
+		buffer = ByteBuffer.allocate(4);
+		buffer.putInt(size + 5 + 11);
+		buffer.flip();
+		output.write(buffer);
 	}
 }
