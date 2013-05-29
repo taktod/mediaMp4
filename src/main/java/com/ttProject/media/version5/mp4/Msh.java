5ed0750943ebe52ae1b01cf8ec9c231580391dec
taktod <poepoemix@hotmail.com>
2013-05-30 08:07:47 +0900

    パッケージ変更

--- src/main/java/com/ttProject/media/version5/mp4/Msh.java
+++ src/main/java/com/ttProject/media/version5/mp4/Msh.java
@@ -1,20 +1,27 @@
-package com.ttProject.media.version5;
+package com.ttProject.media.version5.mp4;
 
 import com.ttProject.media.mp4.Atom;
 import com.ttProject.media.mp4.IAtomAnalyzer;
 import com.ttProject.nio.channels.IFileReadChannel;
 
+/**
+ * mediaSequenceHeader用のatom
+ * @author taktod
+ */
 public class Msh extends Atom {
+	/**
+	 * コンストラクタ
+	 * @param size
+	 * @param position
+	 */
 	public Msh(int size, int position) {
 		super("msh ", size, position);
 	}
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void analyze(IFileReadChannel ch, IAtomAnalyzer analyzer)
-			throws Exception {
-
-	}
-	@Override
-	public String toString() {
-		return super.toString("  ");
+		throws Exception {
 	}
 }
